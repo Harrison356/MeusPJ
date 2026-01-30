b0e73128c3bfc442849ea7193bbd21e4b926f945
Harrison.S <142624422+Harrison356@users.noreply.github.com>
2026-01-29 23:44:21 -0300

    up python

---
+++ Pcote_MeusPJ/Python/Python-01/bigger_smaller.py
@@ -0,0 +1,9 @@
+import os
+
+number1 = int(input("Digite o primeiro numero: "))
+number2 = int(input("Digite o segundo numero: "))
+os.system("cls")
+if number1 > number2:
+    print(number1)
+else:
+    print(number2)
---
+++ Pcote_MeusPJ/Python/Python-01/media.py
@@ -0,0 +1,6 @@
+number = int(input("Digite uma nota de (0 a 10): "))
+
+if number >= 6:
+    print("APROVADO")
+else:
+    print("REPROVADO")
